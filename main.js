7c420355f7b99b518bd7386e99de212324a8ed14
Gustavo <garancibiacl@gmail.com>
2024-03-10 23:30:41 -0300

    se incluye badge en etiqueta li

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ function guardarTareas(tareaPorCategoria) {
         tareaPorCategoria[category].forEach(function (task) {
           let li = document.createElement('li');
           li.innerHTML = `
-            <span>${task.text} (${category})</span>
+            <span  >${task.text} <span class="badge text-bg-primary">${category}</span> </span>
             <button class="text-bg-danger delete-btn tooltip-container" onclick="eliminarTarea('${category}', ${tareaPorCategoria[category].indexOf(task)})"><i class="fas fa-trash"></i><span class="tooltip-text">Eliminar</button>`;
           listaDeTareas.appendChild(li);
         });
